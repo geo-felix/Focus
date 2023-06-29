e5f0db02dbe30db92d1967e88d9a862992314574
GeoFelix <felixodhiambo538@gmail.com>
2023-06-29 22:55:49 +0300

    Testing the sleep method

---
+++ timer.py
@@ -0,0 +1,14 @@
+import time
+
+def focus_time(user_time):
+    while user_time:
+        min, secs= divmod(user_time, 60)
+        timer= '{:02d}:{:02d}'.format(min,secs)
+        print(timer,end="\r")
+        time.sleep(1)
+        user_time -=1
+    print(f"You coded for time")
+
+user_time=int(input("Enter the time in seconds: "))
+
+focus_time(user_time)
